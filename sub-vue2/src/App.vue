f76252da987ca10d054fc3db664290826f8c0ad0
soakit <liuchang2024@gmail.com>
2022-05-31 14:15:55 +0800

    feat: vue2 app

--- sub-vue2/src/App.vue
+++ sub-vue2/src/App.vue
@@ -1,37 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-<!-- import router from './router' -->
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
 
-// Vue.config.productionTip = false
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
 
-let instance = null
-function render(props) {
-    instance = new Vue({
-        router,
-        render: h => h(App)
-    }).$mount('#app'); // 这里是挂载到自己的html中  基座会拿到这个挂载后的html 将其插入进去
-}
-
-if (window.__POWERED_BY_QIANKUN__) { // 动态添加publicPath
-    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
-}
-if (!window.__POWERED_BY_QIANKUN__) { // 默认独立运行
-    render();
-}
-
-// 需要暴露接入协议
-export async function bootstrap(props) {
-  console.log('[vue] vue app bootstraped');
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
 };
+</script>
 
-export async function mount(props) {
-  console.log('[vue] props from main framework', props);
-  render(props);
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-
-export async function unmount(props) {
-   instance.$destroy();
-    instance.$el.innerHTML = '';
-    instance = null;
-    router = null;
-}
+</style>
--- sub-vue2/src/main.js
+++ sub-vue2/src/main.js
@@ -1,8 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let instance = null;
+function render(props) {
+  const { container } = props;
+  instance = new Vue({
+    render: (h) => h(App),
+  }).$mount(
+    container ? container.querySelector("#app") : document.querySelector("#app")
+  ); // 这里是挂载到自己的html中  基座会拿到这个挂载后的html 将其插入进去
+}
+
+// 动态添加publicPath
+if (window.__POWERED_BY_QIANKUN__) {
+  window.__webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
+
+// 默认独立运行
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+// 需要暴露接入协议
+export async function bootstrap() {
+  console.log("[vue] vue app bootstraped");
+}
+
+export async function mount(props) {
+  console.log("[vue] props from main framework", props);
+  render(props);
+}
+
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = "";
+  instance = null;
+}
