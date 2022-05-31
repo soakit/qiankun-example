75f0c379cdb57154d6ac676ccf6d0ae042c84cbd
soakit <liuchang2024@gmail.com>
2022-05-31 14:15:38 +0800

    feat: base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { BrowserRouter, Link } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -8,8 +8,8 @@ function App() {
         <Link to="/sub-react">react应用</Link>
         <Link to="/sub-vue2">vue2应用</Link>
       </BrowserRouter>
-      {/* 切换导航， 将微应用渲染container容器中 */}
 
+      {/* 切换导航， 将微应用渲染container容器中 */}
       <div id="container"></div>
     </div>
   );
